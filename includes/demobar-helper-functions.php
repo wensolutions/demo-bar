dd8265b82c77c6c10304cf652a7a831b8e08d6ff
Nilambar Sharma <nilambar@outlook.com>
2015-12-05 08:26:43 +0545

    doc bloc in helper function

--- includes/demobar-helper-functions.php
+++ includes/demobar-helper-functions.php
@@ -9,8 +9,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-function demobar_array_find_by_key( $products, $field, $value ) {
-	foreach( $products as $key => $product ) {
+/**
+ * Find by key in multi-dimensional array.
+ *
+ * @since 1.0.0
+ *
+ * @param array  $array Main array.
+ * @param string $field Field key.
+ * @param string $value Value to compare.
+ * @return string|int|bool If found return matched ID, else return FALSE.
+ */
+function demobar_array_find_by_key( $array, $field, $value ) {
+	foreach( $array as $key => $product ) {
 		if ( $product[ $field ] === $value ){
 			return $key;
 		}
