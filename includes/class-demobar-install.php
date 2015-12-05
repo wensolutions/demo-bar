79834a406e11a8ab54c546530c8ff6468bc4fdbf
Nilambar Sharma <nilambar@outlook.com>
2015-12-05 15:30:23 +0545

    set default options on Activation

--- includes/class-demobar-install.php
+++ includes/class-demobar-install.php
@@ -16,11 +16,44 @@ class DemoBar_Install {
 
 	/**
 	 * Install.
+	 *
+	 * @since 1.0.0
 	 */
 	public static function install() {
 		// Register post types.
 		DemoBar_Post_Types::register_post_types();
 		// Flush rules after install.
 		flush_rewrite_rules();
+		// Set default options.
+		self::set_default_plugin_options();
+	}
+
+	/**
+	 * Set default settings.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function set_default_plugin_options() {
+		$default_settings = self::get_default_settings();
+		if ( ! get_option( 'demobar_options' ) ) {
+			update_option( 'demobar_options', $default_settings );
+		}
+	}
+
+	/**
+	 * Fetch default settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array Defaults options.
+	 */
+	public static function get_default_settings() {
+		$default = array(
+			'logo'                   => '',
+			'show_purchase_button'   => true,
+			'show_responsive_button' => true,
+			'show_close_button'      => true,
+		);
+		return $default;
 	}
 }
