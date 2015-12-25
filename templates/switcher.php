89b6364765abe060597aca7778540bebeb554df8
codearryaas <codearryaas@gmail.com>
2015-12-25 08:23:34 +0000

    Show Responsive setting issue fixed

--- templates/switcher.php
+++ templates/switcher.php
@@ -13,14 +13,14 @@ $demobar_options = get_option( 'demobar_options' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<title><?php bloginfo( 'name' ); ?> - <?php the_title(); ?></title>
-	<link rel="stylesheet" href="<?php echo DEMOBAR_PLUGIN_URL; ?>/third-party/font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="<?php echo DEMOBAR_PLUGIN_URL; ?>/css/front.css">
+	<link rel="stylesheet" href="<?php echo esc_url( DEMOBAR_PLUGIN_URL ); ?>/third-party/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo esc_url( DEMOBAR_PLUGIN_URL ); ?>/css/front.css">
 	<?php if ( isset( $demobar_options['background_color'] ) && ! empty( $demobar_options['background_color'] ) ) : ?>
 		<style>#db-switcher{background-color:<?php echo esc_attr( $demobar_options['background_color'] ); ?>;}</style>
 	<?php endif ?>
 	<script src="<?php echo esc_url( site_url() ); ?>/wp-includes/js/jquery/jquery.js"></script>
 	<script src="<?php echo esc_url( site_url() ); ?>/wp-includes/js/jquery/jquery-migrate.min.js"></script>
-	<script src="<?php echo DEMOBAR_PLUGIN_URL; ?>/js/front.js"></script>
+	<script src="<?php echo esc_url( DEMOBAR_PLUGIN_URL ); ?>/js/front.js"></script>
 </head>
 <?php
 	$current_demo = '';
@@ -46,9 +46,9 @@ if ( false !== $valid_key ) {
 			</div><!-- #branding -->
 		<?php endif ?>
 		<div id="dropdown">
-			<?php 
-			$selected = esc_html( 'Select', 'demo-bar' ); 
-			$dropdown_list = "";
+			<?php
+			$selected = esc_html( 'Select', 'demo-bar' );
+			$dropdown_list = '';
 			?>
 			<?php if ( ! empty( $sites ) ) : ?>
 				<?php $dropdown_list .= '<ul>'; ?>
@@ -61,7 +61,7 @@ if ( false !== $valid_key ) {
 							get_permalink()
 						);
 
-						if( isset( $_GET['demo'] ) && $_GET['demo'] === $site['slug'] ){
+						if ( isset( $_GET['demo'] ) && $_GET['demo'] === $site['slug'] ) {
 							$selected = esc_html( $site['title'] );
 						}
 
@@ -70,14 +70,16 @@ if ( false !== $valid_key ) {
 						$dropdown_list .= '</li>';
 					?>
 				<?php endforeach; ?>
-				<?php $dropdown_list .="</ul>"; ?>
+				<?php $dropdown_list .= '</ul>'; ?>
 			<?php endif ?>
 
 			<?php
-			echo $selected;
-			echo $dropdown_list;
-			?>
+			// Display select box dropdown.
+			echo wp_kses_post( $selected . $dropdown_list ); ?>
+			
 		</div> <!-- #dropdown -->
+		
+		<?php if ( isset( $demobar_options['show_responsive_button'] ) && 1 === $demobar_options['show_responsive_button'] ) :  ?>
 		<div id="responsive">
 			<a title="<?php esc_html_e( 'Desktop', 'demo-bar' ); ?>" rel="resp-desktop" href="#" class="current"><i class="fa fa-desktop fa-lg"></i></a>
 			<a title="<?php esc_html_e( 'Tablet Landscape (1024x768)', 'demo-bar' ); ?>" rel="resp-tablet-landscape" href="#"><i class="fa fa-tablet fa-rotate-270 fa-lg"></i></a>
@@ -85,6 +87,8 @@ if ( false !== $valid_key ) {
 			<a title="<?php esc_html_e( 'Mobile Landscape (480x320)', 'demo-bar' ); ?>" rel="resp-mobile-landscape" href="#"><i class="fa fa-mobile fa-rotate-270 fa-lg"></i></a>
 			<a title="<?php esc_html_e( 'Mobile Portrait (320x480)', 'demo-bar' ); ?>" rel="resp-mobile-portrait" href="#"><i class="fa fa-mobile fa-lg"></i></a>
 		</div>
+		<?php endif; ?>
+		
 		<div id="buttons">
 			<?php if ( isset( $sites[ $current_demo ]['download_url'] ) && ! empty( $sites[ $current_demo ]['download_url'] ) && true === $demobar_options['show_purchase_button'] ) : ?>
 				<a href="<?php echo esc_url( $sites[ $current_demo ]['download_url'] ); ?>" class="btn btn-download"><?php esc_html_e( 'Download', 'demo-bar' ); ?></a>
